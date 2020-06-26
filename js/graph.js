75570cd7c6aca581c8b1b07351a1c3f67168a847
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-06-26 12:53:33 +0430

    Read file totally and add all pages base layout

--- js/graph.js
+++ js/graph.js
@@ -1,9 +1,11 @@
 class Graph {
   constructor(numberOfVertices) {
     this.numberOfVertices = numberOfVertices;
+    this.nodes = ["0"];
     this.AdjList = new Map();
   }
   addVertex(v) {
+    this.nodes.push(v);
     this.AdjList.set(v, []);
   }
   addEdge(a, b) {
--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,24 @@ class Node {
   }
 }
 
-// Make table and add vertices
-const table = new Graph(81);
+// Make maze and add vertices
+const maze = new Graph(81);
 for (let i = 1; i <= 81; i++) {
-  table.addVertex(new Node(i, "none"));
+  maze.addVertex(new Node(i, "none"));
 }
 
-// ********** File reader section **********
-// document.getElementById("fileInput").addEventListener("change", function () {
-//   var fr = new FileReader();
-//   fr.onload = function () {
-//     console.log(fr.result);
-//   };
-
-//   fr.readAsText(this.files[0]);
-// });
-
+//********** File reader section **********
+document.getElementById("fileInput").addEventListener("change", function () {
+  var fr = new FileReader();
+  fr.onload = function () {
+    let inFile = fr.result;
+    inFile = inFile.replace(/\r?\n|/g, "");
+    inFile = inFile.split("");
+    inFile.unshift("0");
+    // Add kinds
+    for (let i = 1; i <= 81; i++) {
+      maze.nodes[i].kind = inFile[i];
+    }
+  };
+  fr.readAsText(this.files[0]);
+});
